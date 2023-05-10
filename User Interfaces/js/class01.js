015dff15afbf775b077024489b4359d85680241d
Stephanie Moreau <stephanie.moreau@johnabbott.qc.ca>
2023-05-10 14:04:24 -0400

    Class 01

---
+++ User Interfaces/js/class01.js
@@ -0,0 +1,56 @@
+// semicolons at the end are optional
+/*
+console.log("I'm in the sample.js file")
+console.log("Ninja cats")
+console.log("are awesome")
+*/
+/*
+// setting up variables
+let uname = "Steph";
+console.log( uname );
+uname = "Bob";
+console.log( uname );
+
+lname = "Adele";
+console.log(lname);
+
+uname = "2";
+console.log(uname);
+lname = 2.254;
+console.log(lname);
+lname = true;
+console.log(lname);
+*//*
+// javascritp does not care about datatypes
+let tmp;
+tmp = "Steph"; console.log(tmp);
+tmp = 22;console.log(tmp);
+tmp = false;console.log(tmp);
+tmp = 'Steph';console.log(tmp);
+tmp = ""; // this is not null
+
+// let tmp; // this would cause an error
+*/
+/*
+// escaping special characters
+console.log("Ninja\n\rCats")
+// alert("Hello\nWorld!")
+console.log("Ninja<br>Cats");
+*/
+/*
+// Falsy values - false, 0, "", undefined, null, NaN,
+if (undefined){ 
+	console.log("true")
+}else {
+	console.log("false");
+}
+*/
+
+// ARRAYS
+let arrDogs = ["Pugs", 3, "Poodle", "Huskies", "Jack Russel", "Mini Pincher", "Labrador", "Golden Retriever", "Labradoodle", "Golden Doodle", 3, 411 ];
+console.log(arrDogs);
+
+// arrDogs = "HotDog"; // because we can!
+
+
+let myNumber = 8;
