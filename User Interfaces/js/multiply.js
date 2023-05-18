975fa77aedeef01b2b4240eeb66363503f912a83
Stephanie Moreau <stephanie.moreau@johnabbott.qc.ca>
2023-05-18 12:01:44 -0400

    In-class exercise

---
+++ User Interfaces/js/multiply.js
@@ -0,0 +1,22 @@
+/* -- multiply.js --
+
+You CAN NOT alter any html. Your function will replace the content of the body tag.
+
+
+- PART 1
+1. Create the function randomTable(). It will generate 2 random numbers that will be used to indicate how many rows and columns to use.
+
+2. Your randomTable() function will call the function showTable(rows, cols) that will take rows and columns as 2 parameters in order to create a simple html multiplication table using the rows and columns provided and append it to body.
+
+
+- PART 2
+1. Create the function promptTable that will prompt the user twice for a number. They will be used to indicate how many rows and columns to use.
+	If either is cancelled, do not do anything.
+	If a number is provided (use isNaN), use its absolute integer value
+	If the information in not a number or 0, use a random number between 1 and 10
+2. Your function will call the function showTable(rows, cols) that will take rows and columns as 2 parameters in order to create a simple html multiplication table using the rows and columns provided and append it to body.
+
+- PART 3 
+1. Add a button with an event listener that will call your promptTable function when clicked
+
+*/
